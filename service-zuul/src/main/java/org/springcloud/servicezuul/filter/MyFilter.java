012218f410011da24cafe32c323b9a751b0b0b03
LiaoPeng <2926272975@qq.com>
2019-04-28 14:35:16 +0800

    添加service-zuul模块作为路由转发和过滤服务

---
+++ service-zuul/src/main/java/org/springcloud/servicezuul/filter/MyFilter.java
@@ -0,0 +1,56 @@
+package org.springcloud.servicezuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.catalina.connector.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @Auther: LiaoPeng
+ * @Date: 2019/4/28
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s>>>%s",request.getMethod(),request.getRequestURL().toString()));
+        String token = request.getParameter("token");
+        if(token == null){
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(Response.SC_UNAUTHORIZED);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        log.info("ok");
+        return null;
+    }
+}
